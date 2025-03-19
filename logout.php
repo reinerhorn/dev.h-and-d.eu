25476e17555bdd1bf21b1834f3542ee06310f294
admin <admin@h-and-d.eu>
2025-03-19 06:18:10 +0100

    git push -u origin main

--- logout.php
+++ logout.php
@@ -1,5 +1,11 @@
 <?php
 session_start();
+session_unset();
 session_destroy();
-header("Location: /admin/login.php");
+
+// Korrekte URL-Konstruktion zur Login-Seite
+$loginPage = dirname($_SERVER['PHP_SELF']) . "/admin/login.php";
+$loginPage = rtrim($loginPage, "/"); // Doppelte Slashes vermeiden
+header("Location: https://" . $_SERVER['HTTP_HOST'] . $loginPage);
 exit();
+?>
