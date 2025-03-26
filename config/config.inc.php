ab51fdacb464f90c97044715fccc5010f6507e22
admin <admin@h-and-d.eu>
2025-03-26 06:06:01 +0100

    git push --force origin main

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,42 +1,11 @@
 <?php
-<<<<<<< HEAD
-
+/*$mysqli = new mysqli("", "root", "101TanZen101", "dbs06091854");
+if ($mysqli->connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+   exit();
+  }*/
 function getDbConnection()
 {
-  return new mysqli("", "root", "101TanZen101", "dbs060954hd");
+  return new mysqli("", "root", "101TanZen101", "dbs06091954");
 } 
 ?>
-=======
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-
-$host = "localhost"; 
-$user = "root"; 
-$pass = "101TanZen101"; 
-$dbname = "cms_database"; 
-
-$conn = new mysqli($host, $user, $pass, $dbname);
-
-if ($conn->connect_error) {
-    die("Datenbankverbindung fehlgeschlagen: " . $conn->connect_error);
-}
-
-function getUserRole($user_id) {
-    global $conn;
-    $stmt = $conn->prepare("SELECT role FROM plugin_login_users WHERE id = ?");
-    $stmt->bind_param("i", $user_id);
-    $stmt->execute();
-    $stmt->bind_result($role);
-    $stmt->fetch();
-    $stmt->close();
-    return $role ?? null;
-}
-
- 
- 
- 
-
-?>
->>>>>>> 25476e17555bdd1bf21b1834f3542ee06310f294
