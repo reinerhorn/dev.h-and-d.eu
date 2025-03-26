8f285178bb07ec7d31bf990e89759980af40855f
admin <admin@h-and-d.eu>
2025-03-26 02:18:41 +0100

    git push -u origin main

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,9 +1,5 @@
 <?php
-/*$mysqli = new mysqli("", "root", "101TanZen101", "dbs06091854");
-if ($mysqli->connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli->connect_error;
-   exit();
-  }*/
+
 function getDbConnection()
 {
   return new mysqli("", "root", "101TanZen101", "dbs060954hd");
