02fd9afed185dfc610132470cf8a4c822b99aa6f
Reiner Horn <reinerhorn@Reiners-iMac-2.local>
2025-03-29 07:33:42 +0100

    Meine Änderungen

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,17 +1,19 @@
 <?php
 
 function getDbConnection() {
-  static $connection = null;
+    static $connection = null;
 
-  if ($connection === null) {
-      $connection = new mysqli("localhost", "root", "101TanZen101", "dbs060954hd");
-      if ($connection->connect_error) {
-          error_log("Datenbankverbindungsfehler: " . $connection->connect_error);
-          die("<h1>Datenbank nicht erreichbar</h1>");
-      }
-  }
-  return $connection;
+    if ($connection === null) {
+        $config = require '/etc/hd_config.php'; // Sicher außerhalb des Webroots speichern!
+
+        $connection = new mysqli($config['db_host'], $config['db_user'], $config['db_pass'], $config['db_name']);
+
+        if ($connection->connect_error) {
+            error_log("Datenbankverbindungsfehler: " . $connection->connect_error);
+            die("<h1>Datenbank nicht erreichbar</h1>");
+        }
+    }
+    return $connection;
 }
- 
 
 ?>
